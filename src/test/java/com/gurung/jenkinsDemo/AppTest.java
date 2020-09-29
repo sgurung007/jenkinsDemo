000b1e4def5b4606238326156107d654e3ef314d
suraj gurung <sgrg007@gmail.com>
2020-09-29 02:00:52 -0500

    alpha 1

--- src/test/java/com/gurung/jenkinsDemo/AppTest.java
+++ src/test/java/com/gurung/jenkinsDemo/AppTest.java
@@ -3,6 +3,10 @@ package com.gurung.jenkinsDemo;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * Unit test for simple App.
@@ -12,9 +16,29 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+	
+	
+	WebDriver driver;
     @Test
     public void shouldAnswerWithTrue()
     {
+    	WebDriverManager.chromedriver().setup();
+    	driver=new ChromeDriver();
+    	driver.manage().window().maximize();
+    	driver.get("https://www.bbc.com/");
+    	String titleBBC=driver.getTitle();
+    	System.out.println(titleBBC);
+    	
+    	
+    	try {
+    		Thread.sleep(5000);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
         assertTrue( true );
+        
+        driver.close();
+        driver.quit();
+        
     }
 }
